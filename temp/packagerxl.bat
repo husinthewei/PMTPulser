22837a92cfe54f554d927761c459efc9c5b6c9b3
Weiwei Hu <wae3wae@yahoo.com>
2017-01-13 17:35:55 -0500

    Followed Manny's suggestions

--- temp/packagerxl.bat
+++ temp/packagerxl.bat
@@ -1,4 +1,4 @@
 echo off
-rem PXL running in feedback mode
-rem netassembler
-pxl.exe -proj "C:\Users\HEP\Documents\GitHub\PMTPulser\pmtpulser.cpm" -nonetassembler -nosyncannotate
+netassembler
+concept2cm -forward -export -proj "C:\Users\HEP\Documents\GitHub\PMTPulser\pmtpulser.cpm"
+pxl.exe -proj "C:\Users\HEP\Documents\GitHub\PMTPulser\pmtpulser.cpm" -nonetassembler
